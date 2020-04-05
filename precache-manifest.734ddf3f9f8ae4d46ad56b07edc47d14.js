e897de305a89a53c5b595c988f3fb196593848b5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-05 17:50:53 +0000

    Deploy akhilmhdh/sortified to github.com/akhilmhdh/sortified.git:gh-pages

---
+++ precache-manifest.734ddf3f9f8ae4d46ad56b07edc47d14.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ff98a3724021d7ab38c55196417198",
+    "url": "/sortified/index.html"
+  },
+  {
+    "revision": "6eee19e63c23f17f1b97",
+    "url": "/sortified/static/css/main.e446ac37.chunk.css"
+  },
+  {
+    "revision": "2675047384b209184092",
+    "url": "/sortified/static/js/2.38e9f100.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sortified/static/js/2.38e9f100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eee19e63c23f17f1b97",
+    "url": "/sortified/static/js/main.3aeb2506.chunk.js"
+  },
+  {
+    "revision": "decd939c4ae320eb51c5",
+    "url": "/sortified/static/js/runtime-main.1f9a273e.js"
+  },
+  {
+    "revision": "6d018e22bec450f82446dbdd3842fba8",
+    "url": "/sortified/static/media/harabara-webfont.6d018e22.woff2"
+  },
+  {
+    "revision": "fe5272c51d5a87f4789c0edba45e38dc",
+    "url": "/sortified/static/media/harabara-webfont.fe5272c5.woff"
+  },
+  {
+    "revision": "2f06121a8cc2d4ce1ae5f91ae093803a",
+    "url": "/sortified/static/media/poppins-regular-webfont.2f06121a.woff2"
+  },
+  {
+    "revision": "b652b9c78185ad7f57aa0f43dd1ab4e8",
+    "url": "/sortified/static/media/poppins-regular-webfont.b652b9c7.woff"
+  }
+]);
